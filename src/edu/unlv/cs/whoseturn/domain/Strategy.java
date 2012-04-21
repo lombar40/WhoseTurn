95e047907b7798fa6c7fa620336c6749972aaf04
Corbin Benally <benallyc@unlv.nevada.edu>
2012-04-20 22:09:55 -0700

    implemented alpha versions of the drive, chips & salsa, ice cream, and a general random method which is based on the imported java random class

--- src/edu/unlv/cs/whoseturn/domain/Strategy.java
+++ src/edu/unlv/cs/whoseturn/domain/Strategy.java
@@ -5,6 +5,8 @@ import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
+import java.util.Arrays;
+import java.util.Random;
 
 @PersistenceCapable
 public class Strategy {
@@ -43,4 +45,122 @@ public class Strategy {
 	public void setDeleted(Boolean deleted) {
 		this.deleted = deleted;
 	}
-}
+	
+	public void driveRatioOnly() {
+		double[] totalArray;
+		double lowestRatio;
+		String userToDrive;
+		totalArray = new double[numUsersParticipating];
+		double dgRatio = User.getTimesDrove() / User.getTimesGone();
+		
+		for (int i = 0; i < numUsersParticipating; i++){
+			totalArray[i] = User.dgRatio;
+		}
+		Arrays.sort(totalArray);
+		lowestRatio = totalArray[0];
+		userToDrive = lowestRatio.TurnItem.getOwnerKeyString();
+		TurnItem.getSelected() = true;
+	}
+	
+	public void driveRatioWithLies() {
+		double[] totalArray;
+		double lowestRatio;
+		String userToDrive;
+		totalArray = new double[numUsersParticipating];
+		double dlgRatio = User.getTimesDrove() - User.getLies() / User.getTimesGone();
+		
+		for (int i = 0; i < numUsersParticipating; i++){
+			totalArray[i] = User.dgRatio;
+		}
+		Arrays.sort(totalArray);
+		lowestRatio = totalArray[0];
+		userToDrive = lowestRatio.TurnItem.getOwnerKeyString();
+		TurnItem.getSelected() = true;
+	}
+	
+	public void chipsSalsaRatioOnly() {
+		double[] totalArray;
+		double lowestRatio;
+		String userToDrive;
+		totalArray = new double[numUsersParticipating];
+		double csgRatio = User.getChipsPurchased() / User.getTimesGone();
+		
+		for (int i = 0; i < numUsersParticipating; i++){
+			totalArray[i] = User.csgRatio;
+		}
+		Arrays.sort(totalArray);
+		lowestRatio = totalArray[0];
+		userToDrive = lowestRatio.TurnItem.getOwnerKeyString();
+		TurnItem.getSelected() = true;
+	}
+	
+	public void chipsSalsaRatioWithLies() {
+		double[] totalArray;
+		double lowestRatio;
+		String userToDrive;
+		totalArray = new double[numUsersParticipating];
+		double cslgRatio = User.getChipsPurchased() - User.getLies() / User.getTimesGone();
+		
+		for (int i = 0; i < numUsersParticipating; i++){
+			totalArray[i] = User.cslgRatio;
+		}
+		Arrays.sort(totalArray);
+		lowestRatio = totalArray[0];
+		userToDrive = lowestRatio.TurnItem.getOwnerKeyString();
+		TurnItem.getSelected() = true;
+	}
+	
+	public void IceCreamRatioOnly() {
+		double[] totalArray;
+		double lowestRatio;
+		String userToDrive;
+		totalArray = new double[numUsersParticipating];
+		double icgRatio = User.getIceCreamPurchased() / User.getTimesGone();
+		
+		for (int i = 0; i < numUsersParticipating; i++){
+			totalArray[i] = User.icgRatio;
+		}
+		Arrays.sort(totalArray);
+		lowestRatio = totalArray[0];
+		userToDrive = lowestRatio.TurnItem.getOwnerKeyString();
+		TurnItem.getSelected() = true;
+	}
+	
+	public void IceCreamRatioWithLies() {
+		double[] totalArray;
+		double lowestRatio;
+		String userToDrive;
+		totalArray = new double[numUsersParticipating];
+		double iclgRatio = User.getIceCreamPurchased() - User.GetLies() / User.getTimesGone();
+		
+		for (int i = 0; i < numUsersParticipating; i++){
+			totalArray[i] = User.iclgRatio;
+		}
+		Arrays.sort(totalArray);
+		lowestRatio = totalArray[0];
+		userToDrive = lowestRatio.TurnItem.getOwnerKeyString();
+		TurnItem.getSelected() = true;
+	}
+	
+	public void randomUser() {
+		String[] totalArray;
+		double lowestRatio;
+		String userToDrive;
+		totalArray = new String[numUsersParticipating];
+		
+		Random generator = new Random();
+		int randomIndex = generator.nextInt(numUsersParticipating);
+		
+		for (int i = 0; i < numUsersParticipating; i++){
+			totalArray[i] = User.iclgRatio;
+		}
+		
+		
+		
+		Arrays.sort(totalArray);
+		lowestRatio = totalArray[0];
+		userToDrive = lowestRatio.TurnItem.getOwnerKeyString();
+		TurnItem.getSelected() = true;
+	}
+	
+	
