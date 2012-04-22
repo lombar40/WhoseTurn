b12227fcb973122a51d1fba7d44f46230960cd59
JamesOravec <James.Oravec@gmail.com>
2012-04-22 10:25:05 -0700

    Did more clean up, review and documentation.

--- src/edu/unlv/cs/whoseturn/domain/Badge.java
+++ src/edu/unlv/cs/whoseturn/domain/Badge.java
@@ -7,90 +7,183 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
+// TODO JAO The following looks like it has information that is from the database.
+//		I think the design of this is a bit goofy, but if we are going to go down this
+//		road, then we need to make sure that the information is properly initialized in the database.
 /**
- * Class used to represent Badges that are awarded to users. 
+ * Class used to represent Badges that are awarded to users.
+ * 
+ * List of Badges: (Extra returns are to allow autoformat to keep things aligned properly.)
+ * 
+ * BadgeID | Badge Name | Badge Criterion
+ * 
+ * 1000 | Jackass | User submitted a turn with only himself.
+ * 
+ * 1001 | Corner Stone | Selected out of group of 4
+ * 
+ * 1002 | Don't Cross The Streams | Not selected out of group of 4
+ * 
+ * 1003 | Human Sacrifice | Selected out of group of 5
+ * 
+ * 1004 | Not The Thumb | Not selected out of group of 5
+ * 
+ * 1005 | Six Minute Abs | Selected out of group of 6
+ * 
+ * 1006 | Pick Up Sticks | Not selected out of group of 6
+ * 
+ * 1007 | Crapped Out | Selected out of group of 7
+ * 
+ * 1008 | Lucky No. 7 | Not selected out of group of 7
+ * 
+ * 1009 | Snow White | Selected out of group of 8
+ * 
+ * 1010 | Dwarf | Not selected out of group of 8
+ * 
+ * 1011 | FML | Selected out of group of more than 8
+ * 
+ * 1012 | Statistically Speaking | Not selected out of group of more than 8
+ * 
+ * 1013 | Saint | User has no lies for 50 turns.
+ * 
+ * 1014 | Socialite | User is part of a turn with more than 10 people
+ * 
+ * 1020 | StormShadow | User is Chris Jones
+ * 
+ * 1021 | MythBusters | User is Matthew Sowders
  */
-/////////////////////////////////////// ** List of Badges ** ///////////////////////////////////////
-////	BadgeID		Badge Name					Badge Criterion									////		
-////	1000		Jackass						User submitted a turn with only himself.		////
-////	1001		Corner Stone				Selected out of group of 4						////
-////	1002		Don't Cross The Streams		Not selected out of group of 4					////
-////	1003		Human Sacrifice				Selected out of group of 5						////
-////	1004		Not The Thumb				Not selected out of group of 5					////
-////	1005		Six Minute Abs				Selected out of group of 6						////
-////	1006		Pick Up Sticks				Not selected out of group of 6					////
-////	1007		Crapped Out					Selected out of group of 7						////
-////	1008		Lucky No. 7					Not selected out of group of 7					////
-////	1009		Snow White					Selected out of group of 8						////
-////	1010		Dwarf						Not selected out of group of 8					////
-////	1011		FML							Selected out of group of more than 8			////
-////	1012		Statistically Speaking		Not selected out of group of more than 8		////
-////	1013		Saint						User has no lies for 50 turns.					////
-////	1014		Socialite					User is part of a turn with more than 10 people	////
-////	1020		StormShadow					User is Chris Jones								////
-////	1021		MythBusters					User is Matthew Sowders							////
-////																							////
-////////////////////////////////////////////////////////////////////////////////////////////////////
 @PersistenceCapable
 public class Badge {
 
+	/**
+	 * This is the primary key for the badge.
+	 */
 	@PrimaryKey
 	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-	@Extension(vendorName="datanucleus", key="gae.encoded-pk", value="true")
+	@Extension(vendorName = "datanucleus", key = "gae.encoded-pk", value = "true")
 	private String keyString;
-	
+
+	/**
+	 * Used for reference purposes.
+	 */
 	@Persistent
 	private Integer badgeId;
-	
-	@Persistent
-    private String badgeName;
 
+	/**
+	 * The name of the badge. e.g. Jackass (for Jackass badge)
+	 */
 	@Persistent
-    private String badgeCriteria;
+	private String badgeName;
 
+	/**
+	 * A description about the badge. E.g. How it is awarded.
+	 */
 	@Persistent
-    private Boolean deleted;
-	
+	private String badgeCriteria;
+
+	/**
+	 * Soft delete status. true for deleted, false for not deleted.
+	 */
+	@Persistent
+	private Boolean deleted;
+
+	/**
+	 * Persistence manager, used for CRUD operations.
+	 */
 	PersistenceManager pm = PMF.get().getPersistenceManager();
-	
-	
+
 	// Getters and Setters.
+	/**
+	 * Gets the keyString.
+	 * 
+	 * @return keyString as a string.
+	 */
 	public String getKeyString() {
 		return keyString;
 	}
 
+	/**
+	 * Sets teh keyString.
+	 * 
+	 * @param keyString
+	 *            The key string as a string.
+	 */
 	public void setKeyString(String keyString) {
 		this.keyString = keyString;
 	}
 
+	/**
+	 * Gets the badge name as a string.
+	 * 
+	 * @return The badge name.
+	 */
 	public String getBadgeName() {
 		return badgeName;
 	}
 
+	/**
+	 * Sets the badge name.
+	 * 
+	 * @param badgeName
+	 *            The badge name.
+	 */
 	public void setBadgeName(String badgeName) {
 		this.badgeName = badgeName;
 	}
 
+	/**
+	 * Gets the description of how the badge is awarded.
+	 * 
+	 * @return The description.
+	 */
 	public String getBadgeCriteria() {
 		return badgeCriteria;
 	}
 
+	/**
+	 * Sets the criteria of how a badge is awarded.
+	 * 
+	 * @param badgeCriteria
+	 *            The description.
+	 */
 	public void setBadgeCriteria(String badgeCriteria) {
 		this.badgeCriteria = badgeCriteria;
 	}
 
+	/**
+	 * Gets the deleted state.
+	 * 
+	 * @return true for deleted, false for not deleted.
+	 */
 	public Boolean getDeleted() {
 		return deleted;
 	}
 
+	/**
+	 * 
+	 * Sets the deleted state.
+	 * 
+	 * @param deleted
+	 *            true for deleted.
+	 */
 	public void setDeleted(Boolean deleted) {
 		this.deleted = deleted;
 	}
 
+	/**
+	 * Gets the badge id reference number.
+	 * 
+	 * @return The badge id.
+	 */
 	public Integer getBadgeId() {
 		return badgeId;
 	}
 
+	/**
+	 * Sets the badge id reference number.
+	 * 
+	 * @param badgeId
+	 *            The badge id.
+	 */
 	public void setBadgeId(Integer badgeId) {
 		this.badgeId = badgeId;
 	}
--- src/edu/unlv/cs/whoseturn/domain/Category.java
+++ src/edu/unlv/cs/whoseturn/domain/Category.java
@@ -8,79 +8,163 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
+/**
+ * Categories of Whose Turn. E.g. Driving, coffee, ice cream, etc. 
+ */
 @PersistenceCapable
 public class Category {
 
+	/**
+	 * The primary key for category.
+	 */
 	@PrimaryKey
 	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
 	@Extension(vendorName="datanucleus", key="gae.encoded-pk", value="true")
 	private String keyString;
 
+	/**
+	 * Name of the category.
+	 */
 	@Persistent
     private String name;
 
+	/**
+	 * The keyString to the strategy we are using for the category.
+	 */
 	@Persistent
     private String strategyKeyString;
 
+	/**
+	 * An integer for the number of hours between turns that this can occur. Used to avoid duplicate submissions.
+	 */
 	@Persistent
     private int timeBoundaryInHours;
 
+	/**
+	 * Soft delete status. true for deleted.
+	 */
 	@Persistent
     private Boolean deleted;
     
+	 //TODO JAO What is the purpose of this? Need to add java doc.  
 	@Persistent
     private Set<String> turns;
 
 	
 	// Getters and Setters.
+	/**
+	 * Gets the key string.
+	 * 
+	 * @return The key string.
+	 */
 	public String getKeyString() {
 		return keyString;
 	}
 
+	/**
+	 * Sets the keystring.
+	 * 
+	 * @param keyString The key string.
+	 */
 	public void setKeyString(String keyString) {
 		this.keyString = keyString;
 	}
 
+	/**
+	 * Gets the name of the category.
+	 * 
+	 * @return The category name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name of the category.
+	 * 
+	 * @param name The category name.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets the strategy's key string.
+	 * 
+	 * @return The key string of the strategry.
+	 */
 	public String getStrategyKeyString() {
 		return strategyKeyString;
 	}
 
+	/**
+	 * Sets teh strategy keyString that is used by this category.
+	 * 
+	 * @param strategyKeyString The keyString value.
+	 */
 	public void setStrategyKeyString(String strategyKeyString) {
 		this.strategyKeyString = strategyKeyString;
 	}
 
+	/**
+	 * Gets the time boundary.
+	 * 
+	 * @return The time boundary.
+	 */
 	public int getTimeBoundaryInHours() {
 		return timeBoundaryInHours;
 	}
 
+	/**
+	 * Sets the time boundary.
+	 * 
+	 * @param timeBoundaryInHours The time boundary.
+	 */
 	public void setTimeBoundaryInHours(int timeBoundaryInHours) {
 		this.timeBoundaryInHours = timeBoundaryInHours;
 	}
 
+	/**
+	 * Gets the deleted state.
+	 *  
+	 * @return true for deleted.
+	 */
 	public Boolean getDeleted() {
 		return deleted;
 	}
 
+	/**
+	 * Sets the deleted state.
+	 * 
+	 * @param deleted true for deleted.
+	 */
 	public void setDeleted(Boolean deleted) {
 		this.deleted = deleted;
 	}
 
+	/**
+	 * Gets the turns.
+	 * 
+	 * @return The turns.
+	 */
 	public Set<String> getTurns() {
 		return turns;
 	}
 
+	/**
+	 * Sets the turns.
+	 * 
+	 * @param turns The turns.
+	 */
 	public void setTurns(Set<String> turns) {
 		this.turns = turns;
 	}
 	
+	/**
+	 * Adds a turn.
+	 * 
+	 * @param turn The turn to add.
+	 */
 	public void addTurn(Turn turn)
 	{
 		turns.add(turn.getKeyString());
