23cd48bf304f7fdd8a09f31c82338f48d950e370
JamesOravec <James.Oravec@gmail.com>
2012-04-22 10:37:36 -0700

    Simple example of toggle button, on click handling, and check of toggle button state.

--- src/edu/unlv/cs/whoseturn/client/views/desktop/TurnDisplay.java
+++ src/edu/unlv/cs/whoseturn/client/views/desktop/TurnDisplay.java
@@ -6,6 +6,9 @@ import com.google.gwt.user.client.ui.Widget;
 
 import edu.unlv.cs.whoseturn.client.views.AbstractNavigationView;
 import edu.unlv.cs.whoseturn.client.views.NavigationView;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.ToggleButton;
 
 /**
  * Displays the details of a turn.
@@ -19,14 +22,17 @@ public class TurnDisplay extends AbstractNavigationView implements
 	@Override
 	public Widget bodyAsWidget() {
 		// The body of the view.
-		FlowPanel panel = new FlowPanel();
+		
+		AbsolutePanel panel = new AbsolutePanel();
 		panel.setSize("1000px", "500px");
 		
 		Label labelPlaceHolder = new Label();
-		labelPlaceHolder.setText("Turn Display");
 		panel.add(labelPlaceHolder);
+		labelPlaceHolder.setText("Turn Display");
+		
+		ToggleButton tglbtnUpText = new ToggleButton("Toggle Button");
+		panel.add(tglbtnUpText, 65, 51);
 		
 		return panel;
 	}
-
 }
--- src/edu/unlv/cs/whoseturn/client/views/desktop/TurnList.java
+++ src/edu/unlv/cs/whoseturn/client/views/desktop/TurnList.java
@@ -1,7 +1,11 @@
 package edu.unlv.cs.whoseturn.client.views.desktop;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.Widget;
 
 import edu.unlv.cs.whoseturn.client.views.AbstractNavigationView;
@@ -18,12 +22,29 @@ public class TurnList extends AbstractNavigationView implements NavigationView {
 	@Override
 	public Widget bodyAsWidget() {
 		// The body of the view.
-		FlowPanel panel = new FlowPanel();
+		
+		AbsolutePanel panel = new AbsolutePanel();
 		panel.setSize("1000px", "500px");
 		
 		Label labelPlaceHolder = new Label();
-		labelPlaceHolder.setText("Turn List");
 		panel.add(labelPlaceHolder);
+		labelPlaceHolder.setText("Turn List");
+		
+		final ToggleButton toggleButton = new ToggleButton("Toggle Button");
+		panel.add(toggleButton, 65, 51);
+		toggleButton.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				if(toggleButton.isDown()){
+					System.out.println("Button is down yo.");
+				} else {
+					System.out.println("Up yo button.");
+				}
+				
+			}
+		});
+		
 		
 		return panel;
 	}
