adbb3c0e114d169362dcb27dc0674bb0db8b90ee
UNLV-ShaneD <dieckma3@unlv.nevada.edu>
2012-04-24 23:44:37 -0700

    added logout feature, functioning result page

--- src/edu/unlv/cs/whoseturn/mobile/MobileSelectionResultController.java
+++ src/edu/unlv/cs/whoseturn/mobile/MobileSelectionResultController.java
@@ -1,6 +1,7 @@
 package edu.unlv.cs.whoseturn.mobile;
 
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import edu.unlv.cs.whoseturn.domain.Category;
 import edu.unlv.cs.whoseturn.domain.PMF;
+import edu.unlv.cs.whoseturn.domain.User;
 import edu.unlv.cs.whoseturn.server.TurnServiceImpl;
 
 public class MobileSelectionResultController extends MobileCategoryScreenController {
@@ -41,14 +43,25 @@ public class MobileSelectionResultController extends MobileCategoryScreenControl
 	private void doStuff(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		modelCategory(request, response);
 		PersistenceManager manager = PMF.get().getPersistenceManager();
-        List<edu.unlv.cs.whoseturn.domain.User> selectedUsers = getSelectedUsers(request, manager);
+        List<User> selectedUsers = getSelectedUsers(request, manager);
         
         Category category = getCategory(request, response);
         
         TurnServiceImpl turnService = new TurnServiceImpl();
-        List<edu.unlv.cs.whoseturn.domain.User> activeUsers = turnService.findDriver(selectedUsers, category);
+        List<User> activeUsers = turnService.findDriver(selectedUsers, category);
         
         request.setAttribute("activeUsers", activeUsers);
+        
+        List<User> inactiveUsers = new LinkedList<User>();
+        for (User user : selectedUsers) {
+        	if (activeUsers.contains(user)) {
+        		continue;
+        	}
+        	
+        	inactiveUsers.add(user);
+        }
+        
+        request.setAttribute("inactiveUsers", inactiveUsers);
 	}
 
 }
