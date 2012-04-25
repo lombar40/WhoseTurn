24f9818c645cfd0b1ec72db63cffdc2f2e774c4c
Ryan Lombardo <lombar40@unlv.nevada.edu>
2012-04-24 21:52:47 -0700

    Extreme Programming Ryan Lombardo, James Oravec, Dane Strong, James Dabinett
    -Fixed HTML tag that caused some browser to not load the site
    -More work on the profile display

--- src/edu/unlv/cs/whoseturn/client/views/desktop/UserProfileDisplay.java
+++ src/edu/unlv/cs/whoseturn/client/views/desktop/UserProfileDisplay.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import edu.unlv.cs.whoseturn.client.UsersService;
@@ -13,6 +14,10 @@ import edu.unlv.cs.whoseturn.client.views.AbstractNavigationView;
 import edu.unlv.cs.whoseturn.client.views.NavigationView;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
 
 /**
  * Displays a User's profile.
@@ -55,12 +60,12 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 		final Label emailValue = new Label("");
 		panel.add(emailValue, 129, 88);
 
-		Image cornerStone = new Image("images/CornerStone50x50trans.png");
+		Image cornerStone = new Image("images/badges/CornerStone50x50trans.png");
 		cornerStone.setAltText("Corner Stone");
 		panel.add(cornerStone, 206, 133);
 		cornerStone.setSize("50px", "50px");
 
-		Image snowWhite = new Image("images/badges/SnowWhite50x50.jpg");
+		Image snowWhite = new Image("images/badges/SnowWhite50x50trans.png");
 		snowWhite.setAltText("Snow White");
 		panel.add(snowWhite, 206, 274);
 		snowWhite.setSize("50px", "50px");
@@ -69,7 +74,7 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 		panel.add(veteran, 133, 416);
 		veteran.setSize("50px", "50px");
 
-		Image crappedOut = new Image("images/badges/CrappedOut50x50.jpg");
+		Image crappedOut = new Image("images/badges/CrappedOut50x50trans.png");
 		crappedOut.setAltText("Crapped Out - Holy Crap!");
 		panel.add(crappedOut, 342, 201);
 		crappedOut.setSize("50px", "50px");
@@ -79,7 +84,7 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 		panel.add(mythBusters, 406, 133);
 		mythBusters.setSize("50px", "50px");
 
-		Image socialite = new Image("images/badges/Socialite50x50.jpg");
+		Image socialite = new Image("images/badges/Socialite50x50trans.png");
 		socialite.setAltText("Socialite");
 		panel.add(socialite, 275, 343);
 		socialite.setSize("50px", "50px");
@@ -88,12 +93,12 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 		panel.add(elite, 206, 416);
 		elite.setSize("50px", "50px");
 
-		Image fML = new Image("images/badges/FML50x50.jpg");
+		Image fML = new Image("images/badges/FML50x50trans.png");
 		fML.setAltText("FML");
 		panel.add(fML, 342, 274);
 		fML.setSize("50px", "50px");
 
-		Image saint = new Image("images/badges/Saint50x50.jpg");
+		Image saint = new Image("images/badges/Saint50x50trans.png");
 		saint.setAltText("Saint");
 		panel.add(saint, 206, 343);
 		saint.setSize("50px", "50px");
@@ -103,25 +108,25 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 		panel.add(stormShadow, 406, 201);
 		stormShadow.setSize("50px", "50px");
 
-		Image dwarf = new Image("images/badges/Dwarf50x50.jpg");
+		Image dwarf = new Image("images/badges/Dwarf50x50trans.png");
 		panel.add(dwarf, 275, 274);
 		dwarf.setSize("50px", "50px");
 
-		Image luckyNo7 = new Image("images/badges/LuckNo750x50.jpg");
+		Image luckyNo7 = new Image("images/badges/LuckNo750x50trans.png");
 		panel.add(luckyNo7, 135, 274);
 		luckyNo7.setSize("50px", "50px");
 
-		Image humanSacrifice = new Image("images/badges/HumanSacrifice50x50.jpg");
+		Image humanSacrifice = new Image("images/badges/HumanSacrifice50x50trans.png");
 		humanSacrifice.setAltText("Human Sacrifice");
 		panel.add(humanSacrifice, 342, 133);
 		humanSacrifice.setSize("50px", "50px");
 
-		Image sixMinuteAbs = new Image("images/badges/SixMinuteAbs50x50.jpg");
+		Image sixMinuteAbs = new Image("images/badges/SixMinuteAbs50x50trans.png");
 		sixMinuteAbs.setAltText("Six Minute Abs");
 		panel.add(sixMinuteAbs, 206, 201);
 		sixMinuteAbs.setSize("50px", "50px");
 
-		Image teamCheater = new Image("images/badges/TeamCheater50x50.jpg");
+		Image teamCheater = new Image("images/badges/TeamCheater50x50trans.png");
 		teamCheater.setAltText("Team Cheater");
 		panel.add(teamCheater, 342, 416);
 		teamCheater.setSize("50px", "50px");
@@ -130,19 +135,19 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 		panel.add(rookie, 342, 343);
 		rookie.setSize("50px", "50px");
 
-		Image notTheThumb = new Image("images/badges/NotTheThumb50x50.jpg");
+		Image notTheThumb = new Image("images/badges/NotTheThumb50x50trans.png");
 		panel.add(notTheThumb, 135, 201);
 		notTheThumb.setSize("50px", "50px");
 
-		Image pickUpSticks = new Image("images/badges/PickUpSticks50x50.jpg");
+		Image pickUpSticks = new Image("images/badges/PickUpSticks50x50trans.png");
 		panel.add(pickUpSticks, 275, 201);
 		pickUpSticks.setSize("50px", "50px");
 
-		Image crossStreams = new Image("images/badges/DontCrossTheStreams50x50.jpg");
+		Image crossStreams = new Image("images/badges/DontCrossTheStreams50x50trans.png");
 		panel.add(crossStreams, 275, 133);
 		crossStreams.setSize("50px", "50px");
 
-		Image statSpeaking = new Image("images/badges/StatisticallySpeaking50x50.jpg");
+		Image statSpeaking = new Image("images/badges/StatisticallySpeaking50x50trans.png");
 		panel.add(statSpeaking, 133, 343);
 		statSpeaking.setSize("50px", "50px");
 
@@ -150,7 +155,16 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 		panel.add(whoseTurnMaster, 275, 416);
 		whoseTurnMaster.setSize("50px", "50px");
 
-		Image jackass = new Image("images/badges/JackAss50x50.jpg");
+		Image jackass = new Image("images/badges/Jackass50x50trans.png");
+		jackass.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				MyPopup myPopup = new MyPopup("WAT");
+				myPopup.setPopupPosition(event.getClientX(), event.getClientY());
+				myPopup.show();
+				
+			}
+		});
 		panel.add(jackass, 133, 133);
 		jackass.setAltText("Jack Ass");
 		jackass.setSize("50px", "50px");
@@ -163,7 +177,6 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 
 			@Override
 			public void onSuccess(List<String[]> result) {
-				System.out.println(result.get(0)[0]);
 				userNameValue.setText(result.get(0)[0]);
 				emailValue.setText(result.get(0)[1]);
 			}
@@ -180,5 +193,26 @@ public class UserProfileDisplay extends AbstractNavigationView implements Naviga
 	 */
 	public void setUsername(String userName) {
 		this.userName = userName;
+	}
+
+	/**
+	 * Pops up a box upon click to display information.
+	 * Reference: http://www.gwtapps.com/doc/html/com.google.gwt.user.client.ui.PopupPanel.html
+	 */
+	private static class MyPopup extends PopupPanel {
+
+		public MyPopup(String content) {
+			// PopupPanel's constructor takes 'auto-hide' as its boolean
+			// parameter.
+			// If this is set, the panel closes itself automatically when the
+			// user
+			// clicks outside of it.
+			super(true);
+
+			// PopupPanel is a SimplePanel, so you have to set it's widget
+			// property to
+			// whatever you want its contents to be.
+			setWidget(new Label(content));
+		}
 	}
 }
